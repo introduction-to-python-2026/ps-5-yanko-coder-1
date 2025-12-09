b43f1a380feb397fbc97dc7e66872ecd50622521
yanko-coder <yannaykohn@gmail.com>
2025-12-09 21:41:08 +0200

    Add import statements for sympy functions

--- equation_utils.py
+++ equation_utils.py
@@ -1,5 +1,5 @@
 # Add the import statements for necessary sympy functions here
-
+from sympy import symbols, Eq, solve as sympy_solve
 
 ELEMENTS = [
     'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
